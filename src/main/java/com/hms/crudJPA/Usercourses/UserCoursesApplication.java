097536d219b35a5794d5669dbad7169b55b3473f
yasora <yasora@gmail.com>
2019-09-03 11:48:29 +0530

    initial commit

---
+++ src/main/java/com/hms/crudJPA/Usercourses/UserCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.hms.crudJPA.Usercourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCoursesApplication.class, args);
+	}
+
+}
